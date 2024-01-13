4a26fea6c1a60c9c5c7c2201c10ca947a61925b7
vitormuniz0 <vitormuniz311@gmail.com>
2024-01-12 22:27:41 -0300

    comecando a fazer preira pag

---
+++ delicie/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+import Container from './components/Moleculas/header.tsx';
+
+function App() {
+  return( 
+    <>
+      <Container></Container>
+    </>
+  )
+   
+}
+
+export default App;
